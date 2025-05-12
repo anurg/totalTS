7e402ee0736aeca2004c881e1f57ea9d6aa5a866
Anurag <anurg@yahoo.com>
2025-05-12 12:17:05 +0000

    ....TS

--- src/practice1.ts
+++ src/practice1.ts
@@ -1,17 +1,9 @@
-import { it, expect } from 'vitest';
+import { Logger } from "./test.ts";
 
-it("Should add the numbers together", () => {
-  const calculator = {
-    x: 0,
-    y: 0,
-    add() {
-      return this.x + this.y;
-    },
-    setValues(x:number,y:number) {
-      this.x = x;
-      this.y = y;
-    },
-  };
-  calculator.setValues(1, 2);
-  expect(calculator.add()).toEqual(3);
-})
+const myApp = (logger:Logger)=>{
+  logger.info("INFO")
+  logger.log("LOG")
+  logger.warn("WARN")
+  logger.error("ERROR")
+}
+myApp(Logger);
--- src/test.ts
+++ src/test.ts
@@ -17,3 +17,20 @@ lotteryTicket
     });
 
 const request = fetch("https://restcountries.eu/rest/v2/name/portugal")
+
+export const Logger = {
+	log: (message: string) => {
+		console.log(message);
+	},
+	info: (message: string) => {
+		console.info(`INFO: ${message}`);
+	},
+	warn: (message: string) => {
+		console.warn(`WARNING: ${message}`);
+	},
+	error: (message: string) => {
+		console.error(`ERROR: ${message}`);
+	},
+};
+
+export type Logger = typeof Logger;
