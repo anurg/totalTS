a65148fd6338555ecbcdb5be65b0c0f5a9afb8d8
Anurag <anurg@yahoo.com>
2025-05-13 12:13:31 +0000

    ...TS

--- src/practice1.ts
+++ src/practice1.ts
@@ -1,24 +1,2 @@
-import { Equal, Expect } from '@total-typescript/helpers';
+const num1 = "hello" as any as number
 
-export const programModes = [
-  'group',
-  'announcement',
-  '1on1',
-  'selfDirected',
-  'planned1on1',
-  'plannedSelfDirected',
-] as const;
-
-type AllPrograms = (typeof programModes)[0|1|2|3|4|5];
-
-type test = Expect<
-  Equal<
-    AllPrograms,
-    | 'group'
-    | 'announcement'
-    | '1on1'
-    | 'selfDirected'
-    | 'planned1on1'
-    | 'plannedSelfDirected'
-  >
->;
