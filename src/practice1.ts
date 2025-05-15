afb52545fa82c716b1ded07ee6510ea69a85995c
Anurag <anurg@yahoo.com>
2025-05-15 10:14:09 +0000

    ....TS

--- src/practice1.ts
+++ src/practice1.ts
@@ -1,2 +1,37 @@
-const num1 = "hello" as any as number
+import { Expect, Equal } from "@total-typescript/helpers";
+interface User {
+  id: number;
+  name: string;
+}
+
+const users = [
+  {
+    name: "Waqas",
+  },
+  {
+    name: "Zain",
+  },
+];
+
+
+const usersWithIds: User[] = users.map((user, index) => ({
+  ...user,
+  id: index,
+  age: 30,
+}));
+
+
+const userKeys = usersWithIds.map((user) => {
+  const keys = Object.keys(user);
+
+  type test = Expect<Equal<typeof keys, Array<"id" | "name">>>; // red squiggly line under Equal<>
+  return keys;
+});
+
+const keys = Object.entries(user).forEach(([key, value]) => {
+  // key is string, and value is any
+});
+
+
+
 
