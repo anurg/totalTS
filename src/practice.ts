e8b9ec9db08a36c3f25fa474d89261ae1afa36c5
Anurag <anurg@yahoo.com>
2025-05-16 17:39:31 +0000

    ....TS

--- src/practice.ts
+++ src/practice.ts
@@ -1,19 +1,10 @@
-import { it, expect } from "vitest"
-import { Expect, Equal } from "@total-typescript/helpers"
-
-const concatName = function (user: { first: string, last?: string }) {
-    if (!user.last) {
-        return `${user.first}`
+import { Equal, Expect } from "@total-typescript/helpers";
+declare const DEBUG : {
+    getState: ()=>{
+        id:string
     }
-    return `${user.first} ${user.last}`
 }
 
-it("first and last both are enetered", () => {
-    const result = concatName({ first: "John", last: "Doe" })
-    type test = Expect<Equal<typeof result, string>>
-    expect(result).toEqual("John Doe")
-})
-it("Only passing the value for first name", () => {
-    const result = concatName({ first: "John" })
-    expect(result).toEqual("John")
-})
+const state = DEBUG.getState(); // red squiggly line under DEBUG
+
+type test = Expect<Equal<typeof state, { id: string }>>;
